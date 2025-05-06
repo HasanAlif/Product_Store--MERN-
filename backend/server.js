4836af00f6eb198cca84bb5a3ccf08af44313883
Al-Mehedi Hasan Alif <130339768+HasanAlif@users.noreply.github.com>
2025-05-06 13:31:57 +0600

    Database Setup

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import { connectDB } from './config/db.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+app.get('/products', (req, res) => {
+  res.json({ message: 'Hello from the Products!' });
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(3000, () => {
+  connectDB();
+  console.log('Server is running on port 3000');
+});
+
