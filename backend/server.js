dc017d4d34092e67cc96943165cc92f5b837bb5b
Al-Mehedi Hasan Alif <130339768+HasanAlif@users.noreply.github.com>
2025-05-06 14:34:50 +0600

    API setup for product and store data into database

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,29 @@
 import express from 'express';
 import { connectDB } from './config/db.js';
 import dotenv from 'dotenv';
+import Product from './models/product.model.js';
 
 dotenv.config();
 
 const app = express();
 
-app.post('/products', (req, res) => {
-  res.json({ message: 'Hello from the Products!' });
+app.use(express.json());
+
+
+app.post('/api/products',async (req, res) => {
+  const product = req.body;
+
+  if (!product.name || !product.price || !product.image) {
+    return res.status(400).json({ message: 'All fields are required' });
+  }
+  const newProduct = new Product(product);
+
+  try{
+    await newProduct.save();
+    res.status(201).json({sucess: true, data: newProduct});
+  }catch(error){
+    res.status(500).json({sucess: false, message: "Server Error"});
+  }
 });
 
 app.get('/', (req, res) => {
