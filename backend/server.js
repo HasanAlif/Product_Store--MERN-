aa90b497ba429d0f82d2d00e74db5794ecca2d8a
Al-Mehedi Hasan Alif <130339768+HasanAlif@users.noreply.github.com>
2025-05-08 14:53:34 +0600

    SetUp Frontend

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 app.use("/api/products", productRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   connectDB();
