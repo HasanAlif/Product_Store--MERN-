1ba3bdbc428f655314b3d042f78e5b39fdb1c788
Al-Mehedi Hasan Alif <130339768+HasanAlif@users.noreply.github.com>
2025-05-08 13:46:40 +0600

    SetUp Router

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,7 @@
-import express from 'express';
-import { connectDB } from './config/db.js';
-import dotenv from 'dotenv';
-import Product from './models/product.model.js';
-import mongoose from 'mongoose';
+import express from "express";
+import { connectDB } from "./config/db.js";
+import dotenv from "dotenv";
+import productRoutes from "./routes/product.route.js";
 
 dotenv.config();
 
@@ -10,70 +9,9 @@ const app = express();
 
 app.use(express.json());
 
-
-app.post('/api/products',async (req, res) => {
-  const product = req.body;
-
-  if (!product.name || !product.price || !product.image) {
-    return res.status(400).json({ message: 'All fields are required' });
-  }
-  const newProduct = new Product(product);
-
-  try{
-    await newProduct.save();
-    res.status(201).json({sucess: true, data: newProduct});
-  }catch(error){
-    res.status(500).json({sucess: false, message: "Server Error"});
-  }
-});
-
-app.get('/api/products', async (req, res) => {
-  try {
-    const products = await Product.find({});
-    res.status(200).json({ success: true, data: products });
-  } catch (error) {
-    res.status(500).json({ success: false, message: 'Server error' });
-  }
-});
-
-app.put('/api/products/:id', async (req, res) => {
-  const { id } = req.params;
-  const product = req.body;
-
-  if (!mongoose.Types.ObjectId.isValid(id)) {
-    return res.status(404).json({ message: 'Product not found in database' });
-  }
-
-  try {
-    const updatedProduct = await Product.findByIdAndUpdate(id, product, { new: true });
-    res.status(200).json({ success: true, data: updatedProduct });
-  } catch (error) {
-    res.status(500).json({ success: false, message: 'Server error' });
-  }
-});
-
-app.delete('/api/products/:id', async (req, res) => {
-  const { id } = req.params;
-
-  try{
-    const product = await Product.findById(id);
-    if (!product) {
-      return res.status(404).json({ message: 'Product not found in database' });
-    }
-    await Product.findByIdAndDelete(id);
-    res.status(200).json({ message: 'Product deleted successfully' });
-  }catch (error) {
-    return res.status(500).json({ message: 'Server error' });
-  }
-
-});
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use("/api/products", productRoutes);
 
 app.listen(3000, () => {
   connectDB();
-  console.log('Server is running on port 3000');
+  console.log("Server is running on port 3000");
 });
-
