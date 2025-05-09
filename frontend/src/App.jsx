82952b9341046dbc70d68fd34ba7b406548143ff
Al-Mehedi Hasan Alif <130339768+HasanAlif@users.noreply.github.com>
2025-05-09 23:58:55 +0600

    Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,20 @@
-import { Button } from "@chakra-ui/react"
+import { Box, Button } from "@chakra-ui/react";
+import { Routes, Route } from "react-router-dom";
+
+import CreatePage from "./pages/CreatePage";
+import HomePage from "./pages/HomePage";
+import Navbar from "./components/Navbar";
 
 function App() {
-
   return (
-    <>
-      <Button>Hello</Button>
-    </>
-  )
+    <Box minH={"100vh"}>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+      </Routes>
+    </Box>
+  );
 }
 
-export default App
+export default App;
